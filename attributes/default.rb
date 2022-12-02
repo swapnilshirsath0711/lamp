543e61207028d363ba4aef307c901cf2fdcbf291
Swapnil Shirsath <swapnilshirsath@Swapnils-MacBook-Pro.local>
2022-12-02 15:10:21 +0530

    Moved passwords from data bags to attributes file

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,7 @@ default ['lamp']['index_path'] = '/var/www/html/index.html'
 default ['lamp']['server']['document_root'] = 'var/www/default/public_html'
 default ['lamp']['database']['dbname'] = 'default'
 default ['lamp']['database']['admin_username'] = 'dbadmin'
+
+default ['lamp']['database']['root_password'] = 'root123'
+default ['lamp']['database']['admin_password'] = 'root123'
+
--- recipes/database.rb
+++ recipes/database.rb
@@ -7,7 +7,6 @@
 #
 
 #storing all the passwords from passwords/mysql.json file to the "passwords" variable
-passwords = data_bag_item('passwords', 'mysql')
 
 #
 mysql_client 'default' do
@@ -15,7 +14,7 @@ mysql_client 'default' do
   end
 
  mysql_service 'default' do
-    initial_root_password passwords['root_password']
+    initial_root_password node['lamp']['database']['root_password']
     action [:create, :start]
   end
 
@@ -26,7 +25,7 @@ end
 mysql_connection_info = {
   host: '127.0.0.1'
   username: 'root'
-  password: passwords['root_password']
+  password: node['lamp']['database']['root_password']
 }
 
 mysql_database node['lamp']['database']['dbname'] do
@@ -36,7 +35,7 @@ end
 
 mysql_database_user node['lamp']['databse']['admin_username'] do
   connection mysql_connection_info
-  password passwords['admin_password']
+  password node['lamp']['database']['admin_password']
   database_name node['lamp']['database']['dbname']
   host: '127.0.0.1'
   action [:create, :grant]

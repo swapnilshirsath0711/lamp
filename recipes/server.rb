e764334b03c69aa6c87a8f705fcd6162b32380fc
Swapnil Shirsath <swapnilshirsath@Swapnils-MacBook-Pro.local>
2022-12-02 16:37:54 +0530

    added html file

--- recipes/server.rb
+++ recipes/server.rb
@@ -7,12 +7,16 @@
 #
 
 
-package 'httpd' do
-	action :install
+  package 'httpd' do
+	  action :install
   end
 
+  file 'var/www/html/index.html' do
+	  content "<h1>Hello, Folks!</h1><br>"
+  end 
+
   template node['lamp']['index_path'] do
-	source 'index.erb'
+	  source 'index.erb'
   end
 
   httpd_config 'default' do
@@ -20,5 +24,5 @@ package 'httpd' do
   end
 
   service 'httpd' do
-	action [:start, :enable]
+	  action [:start, :enable]
   end

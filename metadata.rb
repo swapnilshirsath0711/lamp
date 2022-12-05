d31aa1867ad8cbcdfdb9ff3e9f8854f27a8140c0
swapnilshirsath0711 <117908481+swapnilshirsath0711@users.noreply.github.com>
2022-12-05 16:52:50 +0530

    metadata updated

--- metadata.rb
+++ metadata.rb
@@ -9,7 +9,9 @@ chef_version '>= 13.0'
 
 
 depends 'httpd', '~> 0.4'
-
+depends 'mysql', '~> 8.0'
+depends 'database', '~> 6.1'
+depends 'mysql2_chef_gem', '~> 2.0'
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.

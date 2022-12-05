8a55e96e193cc2e84165d973b42c9360afbb1445
Swapnil Shirsath <swapnilshirsath@Swapnils-MacBook-Pro.local>
2022-12-05 10:47:35 +0530

    updated Policyfile

--- recipes/server.rb
+++ recipes/server.rb
@@ -11,13 +11,13 @@
 	  action :install
   end
 
-  file 'var/www/html/index.html' do
+  file '/var/www/html/index.html' do
 	  content "<h1>Hello, Folks!</h1><br>"
   end 
 
-  template node['lamp']['index_path'] do
-	  source 'index.erb'
-  end
+  #template node['lamp']['index_path'] do
+	#  source 'index.erb'
+  #end
 
   httpd_config 'default' do
 	  source 'default.conf.erb'

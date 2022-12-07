eb979fe280ef6522570ce56c9fe301f417b12c88
swapnilshirsath0711 <117908481+swapnilshirsath0711@users.noreply.github.com>
2022-12-07 15:30:22 +0530

    change in chef version

--- metadata.rb
+++ metadata.rb
@@ -5,11 +5,11 @@ license 'All Rights Reserved'
 description 'Installs/Configures lamp'
 long_description 'Installs/Configures lamp'
 version '0.1.0'
-chef_version '>= 13.0'
+chef_version '>= 15.3'
 
 
 depends 'httpd', '~> 0.4'
-depends 'mysql', '~> 5.7'
+depends 'mysql', '~> 8.0'
 depends 'database', '~> 6.1'
 depends 'mysql2_chef_gem', '~> 2.0'
 # The `issues_url` points to the location where issues for this cookbook are

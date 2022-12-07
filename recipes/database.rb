aea0c64385820fb3de3b9a09c711c5289ec7384a
swapnilshirsath0711 <117908481+swapnilshirsath0711@users.noreply.github.com>
2022-12-07 15:55:47 +0530

    uninstall mysql

--- recipes/database.rb
+++ recipes/database.rb
@@ -10,40 +10,40 @@
 
 
 package 'mysql-server' do
-  action :install
+  action :uninstall
 end
 
-mysql_service 'default' do
-   initial_root_password node['lamp']['database']['root_password']
-   action [:create, :start]
- end
+# mysql_service 'default' do
+#    initial_root_password node['lamp']['database']['root_password']
+#    action [:create, :start]
+#  end
 
-mysql_client 'default' do
-    action :create
-  end
+# mysql_client 'default' do
+#     action :create
+#   end
 
 
-mysql2_chef_gem 'default' do
-  action :install
-end
+# mysql2_chef_gem 'default' do
+#   action :install
+# end
 
-mysql_connection_info = {
-  'host'=> '127.0.0.1',
-  'username'=> 'root',
-  'password'=> node['lamp']['database']['root_password']
-}
+# mysql_connection_info = {
+#   'host'=> '127.0.0.1',
+#   'username'=> 'root',
+#   'password'=> node['lamp']['database']['root_password']
+# }
 
-mysql_database node['lamp']['database']['dbname'] do
-  connection mysql_connection_info
-  action :create
-end
+# mysql_database node['lamp']['database']['dbname'] do
+#   connection mysql_connection_info
+#   action :create
+# end
 
-mysql_database_user node['lamp']['databse']['admin_username'] do
-  connection mysql_connection_info
-  password node['lamp']['database']['admin_password']
-  database_name node['lamp']['database']['dbname']
-  host '127.0.0.1'  
-  action [:create, :grant]
-end
+# mysql_database_user node['lamp']['databse']['admin_username'] do
+#   connection mysql_connection_info
+#   password node['lamp']['database']['admin_password']
+#   database_name node['lamp']['database']['dbname']
+#   host '127.0.0.1'  
+#   action [:create, :grant]
+# end
 
 

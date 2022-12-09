64eced4b7eff3df225c7082f60b08079abaca380
swapnilshirsath0711 <117908481+swapnilshirsath0711@users.noreply.github.com>
2022-12-09 15:14:26 +0530

    test

--- recipes/server.rb
+++ recipes/server.rb
@@ -12,16 +12,16 @@
   end
 
   # file '/var/www/html/index.html' do
-  file node['lamp']['index_path'] do
-    action :create
-	  content "<h1>Hello</h1><br>
-    <h2>Welcome to Chef Tutorials!</h2><br>
-    <h3>Hostname : #{node['hostname']}</h3><br>
-    <h3>IP Address : #{node['ipaddress']}</h3><br>
-    <h3>Total Memory : #{node['memory']['total']}</h3><br>
-    <h3>Platform : #{node['platform']}</h3><br>
-    "
-  end 
+  # file node['lamp']['index_path'] do
+  #   action :create
+	#   content "<h1>Hello</h1><br>
+  #   <h2>Welcome to Chef Tutorials!</h2><br>
+  #   <h3>Hostname : #{node['hostname']}</h3><br>
+  #   <h3>IP Address : #{node['ipaddress']}</h3><br>
+  #   <h3>Total Memory : #{node['memory']['total']}</h3><br>
+  #   <h3>Platform : #{node['platform']}</h3><br>
+  #   "
+  # end 
   
   service 'httpd' do
     action [:start, :enable]

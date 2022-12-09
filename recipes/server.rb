85dae4dc04ab3fb344094cff2819d1e0cf662bb7
swapnilshirsath0711 <117908481+swapnilshirsath0711@users.noreply.github.com>
2022-12-09 15:29:21 +0530

    using template instead of file in server.rb

--- recipes/server.rb
+++ recipes/server.rb
@@ -12,24 +12,26 @@
   end
 
   # file '/var/www/html/index.html' do
-  file node['lamp']['index_path'] do
-    action :create
-	  content "<h1>Hello</h1><br>
-    <h2>Welcome to Chef Tutorials!</h2><br>
-    <h3>Hostname : #{node['hostname']}</h3><br>
-    <h3>IP Address : #{node['ipaddress']}</h3><br>
-    <h3>Total Memory : #{node['memory']['total']}</h3><br>
-    <h3>Platform : #{node['platform']}</h3><br>
-    "
-  end 
+  # file node['lamp']['index_path'] do
+  #   action :create
+	#   content "<h1>Hello</h1><br>
+  #   <h2>Welcome to Chef Tutorials!</h2><br>
+  #   <h3>Hostname : #{node['hostname']}</h3><br>
+  #   <h3>IP Address : #{node['ipaddress']}</h3><br>
+  #   <h3>Total Memory : #{node['memory']['total']}</h3><br>
+  #   <h3>Platform : #{node['platform']}</h3><br>
+  #   "
+  # end 
+  
+  # Using template resource instead of file resource to generate dynamic content
+
+  template node['lamp']['index_path'] do
+    source 'index.erb'
+  end
   
   service 'httpd' do
     action [:start, :enable]
   end
-  
-  # template node['lamp']['index_path'] do
-  #   source 'index.erb'
-  # end
 
   # httpd_config 'default' do
 	#   source 'default.conf.erb'

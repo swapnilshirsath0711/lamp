9af35ebf3d7d6a62a51a251aae87b16533bb2b72
swapnilshirsath0711 <117908481+swapnilshirsath0711@users.noreply.github.com>
2022-12-14 18:19:12 +0530

    test

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -1,4 +1,4 @@
-# InSpec test for recipe practice::default
+# InSpec test for recipe lamp::default
 
 # The InSpec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
@@ -7,10 +7,12 @@
     # This is an example test, replace with your own test.
     describe user('root') do
       it { should exist }
+      skip 'This is an example test, replace it with your own test.'
     end
   end
   
   # This is an example test, replace it with your own test.
   describe port(80) do
     it { should be_listening }
+    skip 'This is an example test, replace it with your own test.'
   end

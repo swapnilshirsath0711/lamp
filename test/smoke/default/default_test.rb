c2e4b430bb5eedeeb036ce32a6786fff33defe3a
swapnilshirsath0711 <117908481+swapnilshirsath0711@users.noreply.github.com>
2022-12-15 10:57:17 +0530

    test

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -3,14 +3,12 @@
 # The InSpec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-  unless os.windows?
-    describe user('root') do
-      it { should exist }
-      skip 'This is an example test, replace it with your own test.'
-    end
-  end
+  # unless os.windows?
+  #   describe user('root') do
+  #     it { should exist }
+  #   end
+  # end
   
   describe port(80) do
     it { should be_listening }
-    skip 'This is an example test, replace it with your own test.'
   end
--- test/smoke/default/setup.rb
+++ test/smoke/default/setup.rb
@@ -8,11 +8,9 @@
 unless os.windows?
     describe user('root') do
       it { should exist }
-      skip 'This is an example test, replace with your own test.'
     end
   end
   
   describe port(80) do
     it { should be_listening }
-    skip 'This is an example test, replace with your own test.'
   end

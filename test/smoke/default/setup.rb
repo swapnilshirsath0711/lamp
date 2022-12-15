808a3251eebad0c467b90d0ec1608a8263f0de23
swapnilshirsath0711 <117908481+swapnilshirsath0711@users.noreply.github.com>
2022-12-15 10:34:11 +0530

    test

--- test/smoke/default/setup.rb
+++ test/smoke/default/setup.rb
@@ -5,12 +5,12 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec_reference.html
 
-unless os.windows?
+# unless os.windows?
     describe user('root') do
       it { should exist }
       skip 'This is an example test, replace with your own test.'
     end
-  end
+#   end
   
   describe port(80) do
     it { should_not be_listening }

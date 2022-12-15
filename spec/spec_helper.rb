759df9a829eff4132a9da240504a2533b01ec9c8
swapnilshirsath0711 <117908481+swapnilshirsath0711@users.noreply.github.com>
2022-12-15 17:29:02 +0530

    test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,17 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
+
+require 'spec_helper'
+
+describe 'lamp::setup' do
+  context 'When all attributes are default, on an unspecified platform' do
+    let(:chef_run) do
+      runner = ChefSpec::ServerRunner.new
+      runner.converge(described_recipe)
+    end
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+  end
+end

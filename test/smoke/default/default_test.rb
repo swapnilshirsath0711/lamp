4091df582827b98822fa2408a2f27966597c68f6
swapnilshirsath0711 <117908481+swapnilshirsath0711@users.noreply.github.com>
2022-12-15 10:42:36 +0530

    test

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -4,14 +4,12 @@
 # found at http://inspec.io/docs/reference/resources/
 
   unless os.windows?
-    # This is an example test, replace with your own test.
     describe user('root') do
       it { should exist }
       skip 'This is an example test, replace it with your own test.'
     end
   end
   
-  # This is an example test, replace it with your own test.
   describe port(80) do
     it { should be_listening }
     skip 'This is an example test, replace it with your own test.'
--- test/smoke/default/setup.rb
+++ test/smoke/default/setup.rb
@@ -5,14 +5,14 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec_reference.html
 
-# unless os.windows?
+unless os.windows?
     describe user('root') do
       it { should exist }
       skip 'This is an example test, replace with your own test.'
     end
-#   end
+  end
   
   describe port(80) do
-    it { should_not be_listening }
+    it { should be_listening }
     skip 'This is an example test, replace with your own test.'
   end

847cd6af16d5ccff302898c769855b2a8fab18d1
swapnilshirsath0711 <117908481+swapnilshirsath0711@users.noreply.github.com>
2022-12-15 17:51:27 +0530

    test

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -4,12 +4,23 @@
 # found at http://inspec.io/docs/reference/resources/
 require 'os'
 
-  unless OS.windows?
-    describe user('root') do
-      it { should exist }
-    end
-  end
+  # unless OS.windows?
+  #   describe user('root') do
+  #     it { should exist }
+  #   end
+  # end
   
-  describe port(80) do
-    it { should be_listening }
+  # describe port(80) do
+  #   it { should be_listening }
+  #   its(:protocols) { should include 'tcp' }
+  # end
+
+  describe file('/var/www/default/public_html/index.php') do
+    it { should exist }
+    it { should be_owned_by 'root' }
+    its(:content) {should match '<!DOCTYPE html>' }
+  end
+
+  describe command('curl localhost') do
+    its(:stdout) { should match 'swapnil' }
   end

2b665785dbf3c273b1c8c8f0407ee96a3aabe8a9
swapnilshirsath0711 <117908481+swapnilshirsath0711@users.noreply.github.com>
2022-12-16 13:23:38 +0530

    unit test

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -4,18 +4,19 @@
 #
 # Copyright (c) 2022 The Authors, All Rights Reserved.
 
-# require 'spec_helper'
+require 'spec_helper'
 require 'rspec'
 
 describe 'lamp::default' do
-  context 'When all attributes are default, on an unspecified platform' do
+  context 'When all attributes are default, on an Amazon 2' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::ServerRunner.new(platform: 'amazon', version: '2')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
-      expect { chef_run }.to raise_error
+      expect { chef_run }.to_not raise_error
     end
   end
 end
+

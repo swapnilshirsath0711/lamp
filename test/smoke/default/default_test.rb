1ed177d9bd51665fa749266c38ce7271193327ff
swapnilshirsath0711 <117908481+swapnilshirsath0711@users.noreply.github.com>
2022-12-16 14:58:32 +0530

    test

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -11,13 +11,16 @@ require 'serverspec'
     end
   end
   
-  describe port(80) do
-    it { should be_listening.on('0.0.0.0').with('tcp') }
-    its(:protocols) { should include 'tcp' }
-  end
+  # describe port(80) do
+  #   it { should be_listening.on('0.0.0.0').with('tcp') }
+  #   its(:protocols) { should include 'tcp' }
+  # end
 
   describe port(80) do
+    it { should be_listening }
+    its('processes') {should include 'syslog'}
   end
+
 
   describe file('/var/www/default/public_html/index.php') do
     it { should_not exist }

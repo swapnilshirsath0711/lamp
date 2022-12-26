bb0a30b54deb6c64f66a848d8069cc135e01fc00
swapnilshirsath0711 <117908481+swapnilshirsath0711@users.noreply.github.com>
2022-12-26 16:53:22 +0530

    test

--- Berksfile
+++ Berksfile
@@ -1,3 +1,5 @@
 source 'https://supermarket.chef.io'
 
 metadata
+
+# if current version of chef does not support Berkshelf, we need to add dependencies into Policyfile
--- Policyfile_Swapnil.rb
+++ Policyfile_Swapnil.rb
@@ -13,20 +13,21 @@ default_source :supermarket
 run_list 'lamp::default'
 # named_run_list 'setup_build_server', 'lamp::setup_build_server'
 
-
-cookbook 'mysql', '~> 8.0'
-cookbook 'database', '~> 6.1'
-cookbook 'mysql2_chef_gem', '~> 2.0'
-
-metadata
-depends 'mysql', '~> 8.0'
-depends 'database', '~> 6.1'
-depends 'mysql2_chef_gem', '~> 2.0'
-end
-
-group :database do
-    cookbook 'mysql', '~> 8.0'
-end
-
 # Specify a custom source for a single cookbook:
 cookbook 'lamp', '~> 1.0.0', git: 'git@github.com:swapnilshirsath0711/lamp.git', branch: 'main'
+
+
+# if current version of chef does not support Berkshelf, we need to add dependencies here
+# cookbook 'mysql', '~> 8.0'
+# cookbook 'database', '~> 6.1'
+# cookbook 'mysql2_chef_gem', '~> 2.0'
+
+# metadata
+# depends 'mysql', '~> 8.0'
+# depends 'database', '~> 6.1'
+# depends 'mysql2_chef_gem', '~> 2.0'
+# end
+
+# group :database do
+#     cookbook 'mysql', '~> 8.0'
+# end

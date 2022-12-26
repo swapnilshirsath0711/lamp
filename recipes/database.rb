0cc4bf338553e682e6fc356ffc4f5cde58dfef51
swapnilshirsath0711 <117908481+swapnilshirsath0711@users.noreply.github.com>
2022-12-26 11:27:34 +0530

    adding metadata block in policyfile

--- recipes/database.rb
+++ recipes/database.rb
@@ -36,3 +36,41 @@ mysql_database_user node['lamp']['database']['admin_username'] do
     host '127.0.0.1'
     action [:create, :grant]
 end
+
+
+
+
+# # ============================================================================================================
+# # ------ code from chat.openai.com ---------
+
+# # To setup multiple applications on the same server with different hosts using chef, you can use the 'apache2' cookbook and the 'web_app' resource.
+# # Here's an example of how you might do this:
+#     # ---- code 1 ------
+#         # install the Apache web server
+#         include_recipe 'apache2'
+#         # Set up the first application 
+#         web_app 'app1' do
+#             server_name 'app1.example.com'
+#             server_aliases ['www.app1.example.com']
+#             docroot '/srv/app1/public'
+#         end
+        
+#     # ------code 2 ------
+#         # Set up the second application 
+#         web_app 'app2' do
+#             server_name 'app2.example.com'
+#             server_aliases ['www.app2.example.com']
+#             docroot '/srv/app2/public'
+#         end
+
+# # This code installs the Apache web server and then sets up two applications, 'app1' and 'app2', with different server names and document roots.
+# # The 'server_name' property specifies the hostname for the application, and the 'server_aliases' property allows you to specify additional hostnames that should be routed to the same application.
+# # The 'docroot' property specifies the directory where the application's files are stored.
+# # You can then use the 'service' resource to start and enable  the Apache web server:
+#         # code for 'service' resource
+#         service 'apache2' do
+#             action [:enable, :start]
+#         end
+#     # this will start the Apache web server and enable it to start automatically on boot.
+#     # 
+# # --------------   END  --------------------

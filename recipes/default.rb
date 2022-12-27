7e064ee2d97221fc0f5f7cada372471b7dbc5ce8
swapnilshirsath0711 <117908481+swapnilshirsath0711@users.noreply.github.com>
2022-12-27 17:55:53 +0530

    Test

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,31 +5,5 @@
 # Copyright:: 2022, The Authors, All Rights Reserved.
 #
 
-# include_recipe 'lamp::setup'
-# include_recipe 'lamp::database'
-include_recipe 'lamp::prep'
-include_recipe 'lamp::httpd'
-include_recipe 'lamp::mysql'
-
-# create_tables = ::File.join(Chef::Config[:file_cache_path], 'create-table.sql')
-
-# # cookbook_file '/path/on/node' do
-# cookbook_file create_tables do
-#     source 'create-table.sql'
-# end
-
-# execute "initialize #{node['lamp']['database']['dbname']} database" do
-#     command "mysql -h 127.0.0.1 -u #{node['lamp']['database']['admin_username']} -p#{node['lamp']['database']['admin_password']} -D #{node['lamp']['database']['dbname']} < #{create_tables}"
-#     not_if "mysql -h 127.0.0.1 -u #{node['lamp']['database']['admin_username']} -p#{node['lamp']['database']['admin_password']} -D #{node['lamp']['database']['dbname']} -e 'describe client;'"
-# end
-
-# # guards ---> not_if AND only_if
-# # include_recipe 'lamp::setup_build_server'
-
-# template "#{node['lamp']['setup']['document_root']}/index.php" do
-#     source 'index.php.erb'
-#     variables({
-#         servername: '127.0.0.1',
-#         admin_password: "#{node['lamp']['database']['admin_password']}"
-#     })
-# end
+include_recipe 'lamp::setup'
+include_recipe 'lamp::database'
--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,53 +1,32 @@
-#
 # Cookbook:: lamp
 # Recipe:: setup
-#
 # Copyright:: 2022, The Authors, All Rights Reserved.
 
   package 'httpd' do
 	  action :install
   end
 
-  # file '/var/www/html/index.html' do
-  # file node['lamp']['index_path'] do
-  #   action :create
-	#   content "<h1>Hello</h1><br>
-  #   <h2>Welcome to Chef Tutorials!</h2><br>
-  #   <h3>Hostname : #{node['hostname']}</h3><br>
-  #   <h3>IP Address : #{node['ipaddress']}</h3><br>
-  #   <h3>Total Memory : #{node['memory']['total']}</h3><br>
-  #   <h3>Platform : #{node['platform']}</h3><br>
-  #   "
-  # end 
-  
 #     web_app 'app1' do
 #         server_name 'app1.example.com'
 #         server_aliases ['www.app1.example.com']
 #         docroot '/srv/app1/public'
 #     end
-    
-# # ------code 2 ------
-#     # Set up the second application 
+     # Set up the second application 
 #     web_app 'app2' do
 #         server_name 'app2.example.com'
 #         server_aliases ['www.app2.example.com']
 #         docroot '/srv/app2/public'
 #     end
 
-  # Using template resource instead of file resource to generate dynamic content
+template node['lamp']['index_path'] do
+  source 'index.erb'
+  variables ({ipaddress: node['ipaddress']})
+end
 
-  template node['lamp']['index_path'] do
-    source 'index.erb'
-    variables ({ipaddress: node['ipaddress']})
-  end
-  
-  httpd_service 'app1' do
-    listen_ports ['80']
-    action [:start, :enable]
-  end
-
-  # install, start and enable mysql
-
+httpd_service 'app1' do
+  listen_ports ['80']
+  action [:start, :enable]
+end
 
 package 'mysql-server' do
   action :install
@@ -57,7 +36,6 @@ service 'mysqld' do
   action [:start, :enable]
 end
 
-# install php
 package 'php' do
   action :install
 end
@@ -101,75 +79,4 @@ node['lamp']['servers'].each do |host, port_data|
   
   end
 end
-
-
-# -----------------------------------------------------------------------------------------
-
-# # commenting all above code and using directory resource
-# # setting up httpd configuration. directory "document_root" has html page (index.html) and configuration files for httpd
-
-# directory node['lamp']['setup']['document_root'] do
-#   recursive true
-# end
-
-# # 'httpd_config' resource comes with dependency 'httpd' added in metadata file 
-
-# httpd_config 'default' do
-#   source 'default.conf.erb'
-# end
-
-# # httpd_service resource also comes with dependency 'httpd' (i.e. community cookbook) added in metadata file
-# # mpm 'prefork' is used for php
-
-# httpd_service 'default' do
-#   mpm 'prefork'
-#   action [:create, :start]
-#   subscribes :restart, 'httpd_config[default]'
-# end
-
-# # notifies --> will notify the other resource if there is any change in current resource
-# # subscribes --> will receive the notification from other resource
-
-# # subscribes :restart, 'httpd_config[default]' --> will receive notification when there is any change in 'httpd_config[default]'
-
-
-
-
-
-
-
-
-
-#storing all the passwords from passwords/mysql.json file to the "passwords" variable
-
-
-# mysql_service 'default' do
-#    initial_root_password node['lamp']['database']['root_password']
-#    action [:create, :start]
-#  end
-
-# mysql_client 'default' do
-#     action :create
-#   end
-
-
-# mysql2_chef_gem 'default' do
-#   action :install
-# end
-
-
-
-# mysql_database node['lamp']['database']['dbname'] do
-#   connection mysql_connection_info
-#   action :create
-# end
-
-# mysql_database_user node['lamp']['databse']['admin_username'] do
-#   connection mysql_connection_info
-#   password node['lamp']['database']['admin_password']
-#   database_name node['lamp']['database']['dbname']
-#   host '127.0.0.1'  
-#   action [:create, :grant]
-# end
-
 

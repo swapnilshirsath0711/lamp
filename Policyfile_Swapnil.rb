c0e81150f648a41ba576e853d176f2ae051df134
swapnilshirsath0711 <117908481+swapnilshirsath0711@users.noreply.github.com>
2022-12-28 16:50:41 +0530

    test

--- Policyfile_Swapnil.rb
+++ Policyfile_Swapnil.rb
@@ -15,9 +15,9 @@ default_source :supermarket
 run_list 'lamp::default'
 # named_run_list 'setup_build_server', 'lamp::setup_build_server'
 
-include_policy 'mysql', source: :supermarket
 
 cookbook 'lamp', '~> 1.0.0', git: 'git@github.com:swapnilshirsath0711/lamp.git', branch: 'main'
+include_policy 'mysql', source: :supermarket
 
 # Specify a custom source for a single cookbook:
 

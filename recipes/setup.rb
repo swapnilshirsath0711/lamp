87d07f11cf9701088d0c402eb6c56c3f71df9043
swapnilshirsath0711 <117908481+swapnilshirsath0711@users.noreply.github.com>
2023-01-03 17:12:23 +0530

    test

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -3,64 +3,64 @@
 # Copyright:: 2022, The Authors, All Rights Reserved.
 
 package 'httpd' do
-	action :install
+	action :remove
 end
 
 package 'mysql-server' do
-  action :install
+  action :remove
 end
 
 package 'php' do
-  action :install
+  action :remove
 end
 
-template node['lamp']['index_path'] do
-  source 'index.erb'
-  variables ({ipaddress: node['ipaddress']})
-end
+# template node['lamp']['index_path'] do
+#   source 'index.erb'
+#   variables ({ipaddress: node['ipaddress']})
+# end
 
-service 'httpd' do
-  action [:start, :enable]
-end
+# service 'httpd' do
+#   action [:start, :enable]
+# end
 
-service 'mysqld' do
-  action [:start, :enable]
-end
+# service 'mysqld' do
+#   action [:start, :enable]
+# end
 
-package 'php-mysql' do
-  action :install
-  notifies :restart, 'service[httpd]'
-end
+# package 'php-mysql' do
+#   action :install
+#   notifies :restart, 'service[httpd]'
+# end
 
-node['lamp']['servers'].each do |host, port_data|
-  root_directory = "/var/www/swapnil/#{host}"
+# node['lamp']['servers'].each do |host, port_data|
+#   root_directory = "/var/www/swapnil/#{host}"
   
-  directory "/etc/httpd/swapnil/conf.d" do
-  recursive true
-  end
+#   directory "/etc/httpd/swapnil/conf.d" do
+#   recursive true
+#   end
 
-  template "/etc/httpd/swapnil/conf.d/#{host}.conf" do
-    source "default.conf.erb"
-    mode "0644"
-    variables(
-      :root_directory => root_directory,
-      :port => port_data["port"],
-      :host => host
-    )
-    notifies :restart, "service[httpd]"
-    end
-  directory root_directory do 
-    mode "0755"
-    recursive true
-  end
+#   template "/etc/httpd/swapnil/conf.d/#{host}.conf" do
+#     source "default.conf.erb"
+#     mode "0644"
+#     variables(
+#       :root_directory => root_directory,
+#       :port => port_data["port"],
+#       :host => host
+#     )
+#     notifies :restart, "service[httpd]"
+#     end
+#   directory root_directory do 
+#     mode "0755"
+#     recursive true
+#   end
 
-  template "#{root_directory}/index.html" do
-    source "app.html.erb"
-    mode "0755"
-    variables(
-      :host => host,
-      :port => port_data["port"]
-    )
+#   template "#{root_directory}/index.html" do
+#     source "app.html.erb"
+#     mode "0755"
+#     variables(
+#       :host => host,
+#       :port => port_data["port"]
+#     )
   
-  end
-end
+#   end
+# end
